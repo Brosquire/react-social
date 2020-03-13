14ba0e6cf208d03a8fc82eac1de493c9c1e2a883
Zachary Ferreira <Pearlybrah@github.com>
2020-03-12 23:11:07 -0700

    added routes for adding/deleting comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -186,4 +186,88 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+//@route   POST api/posts/comment/:id
+//@desc    Comment on a post
+//@access  Private
+router.post(
+  "/comment/:id",
+  //setting middleware to authenticate user and check if required data is valid
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //checking if errors occur and logging them if true
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      //setting our user from our User model and disabling the password from being returned
+      const user = await User.findById(req.user.id).select("-password");
+      //setting our post from our Post model
+      const post = await Post.findById(req.params.id);
+      //setting a new comment object
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      //adding new comments to the database using unshift and our newComments object
+      post.comments.unshift(newComment);
+
+      //saving our new post with comments added
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//@route   DELETE api/posts/comment/:id/:comment_id
+//@desc    Delete a comment
+//@access  Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    //setting our post from our Post model
+    const post = await Post.findById(req.params.id);
+
+    //get the comment from the post using the req.params query
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+    //check if comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    //check if user is the user who made the comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User Not Authorized" });
+    }
+
+    //deleting a comment
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    //removing comment from array
+    post.comments.splice(removeIndex, 1);
+    //saving comments to the database
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
