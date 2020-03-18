672e1a011fc4893b00ca60556a342cf93f6b8514
Zachary Ferreira <Pearlybrah@github.com>
2020-03-18 16:09:00 -0700

    setting up production to be deployed

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 //requiring the connectDB function;
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 //initilizing our "App" and setting it to express
 const app = express();
 
@@ -13,13 +15,21 @@ connectDB();
 //Init Middleware - to parse the request of the body in JSON format
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 //Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+//Serve static assets in production
+if (process.env.NODE.ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //setting variable for PORT data to be used || process.env.PORT = deployed PORT key
 const PORT = process.env.PORT || 5000;
